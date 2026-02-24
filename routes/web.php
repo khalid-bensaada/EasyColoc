c07a965b1b70a0b9de2dba921319293d41c6efa3
khalid-bensaada <kbensaada953@gmail.com>
2026-02-24 14:41:54 +0000

    added a routs of profile

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use app\Http\Controllers\ProfileController;
 
 Route::get('/', function () {
     return view('welcome');
@@ -14,4 +15,19 @@ Route::middleware([
     Route::get('/dashboard', function () {
         return view('dashboard');
     })->name('dashboard');
+});
+
+
+Route::middleware(['auth'])->group(function () {
+
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+
+    Route::post('/profile/update', [ProfileController::class, 'update'])->name('profile.update');
+
+    Route::post('/profile/photo', [ProfileController::class, 'updatePhoto'])->name('profile.photo');
+
+    Route::post('/colocation/leave', [ProfileController::class, 'leaveColocation'])->name('colocation.leave');
+
+    Route::post('/colocation/cancel', [ProfileController::class, 'cancelColocation'])->name('colocation.cancel');
+
 });
